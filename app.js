84c9347b79dfe1f6a1f6cb6c2978fae4dbff4630
sophielyall <s3564198@student.rmit.edu.au>
2017-09-17 17:18:45 +1000

    Edited links to pages.

--- app.js
+++ app.js
@@ -6,53 +6,63 @@ var bodyParser = require('body-parser');
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 var CognitoUserAttribute = AmazonCognitoIdentity.CognitoUserAttribute;
-//var Congnito =AWSCognito.CognitoIdentityServiceProvider;
-
 var AuthenticationDetails = AmazonCognitoIdentity.AuthenticationDetails;
+var CognitoUser = AmazonCognitoIdentity.CognitoUser;
 
-
-  var poolData = {
-        UserPoolId : 'ap-southeast-2_XVdm31LEy', // Your user pool id here
-        ClientId : '6kgij187eog3fob8rqjufgiock' // Your client id here
+    //user pool data
+    var poolData = {
+        // Your user pool id here
+        UserPoolId : 'ap-southeast-2_XVdm31LEy', 
+        // Your client id here
+        ClientId : '6kgij187eog3fob8rqjufgiock' 
     };
+
+
     var userPool = new CognitoUserPool(poolData);
 
 
 
 
-var CognitoUser = AmazonCognitoIdentity.CognitoUser; 
-//var cognitoUser = AmazonCognitoIdentity.CognitoUser;
+ 
+
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + '/public')));
-//AmazonCognitoIdentity.config.region = "ap-southeats-2";
+
 var port = 3700 //stores port number to listen on
 
 app.use(express.static(path.join(__dirname + '/css'))); //allows html file to reference stylesheet "helloworld.css" that is stored in ./css directory
 
+//to get login page
 app.get('/', function(req, res) { //on html request of root directory, run callback function
     res.sendFile(path.join(__dirname, '/login.html')); //send html file named "helloworld.html"
 });
 
+
+//access welcome page after login
 app.post('/login', function(req, res){
-    res.sendFile(path.join(__dirname, 'welcome.html'));
+    //res.sendFile(path.join(__dirname, 'welcome.html'));
 
      var aUsername = req.body.username;
     var aPassword = req.body.password;
 
     console.log("User Name: " + aUsername + " Password: "+ aPassword);
 
-    Login(aUsername, aPassword);
+    //login with username and password given
+    Login(aUsername, aPassword, res);
 });
+
+//get register page
 app.get('/register', function(req, res){
 	res.sendFile(path.join(__dirname, 'register.html'));
     
 });
 
+//get confirm page
 app.post('/signup', function(req, res){
-    res.sendFile(path.join(__dirname, 'confirm.html'));
+    //res.sendFile(path.join(__dirname, 'confirm.html'));
     var aName = req.body.name;
     var aFamilyName = req.body.familyName
     var aEmail = req.body.email;
@@ -61,23 +71,23 @@ app.post('/signup', function(req, res){
     var aUserName = req.body.userName;
     var aPassword = req.body.password;
 
-    
-    register(aName, aFamilyName, aEmail, aAddress, aPhone, aUserName, aPassword);
+    //register function
+    register(aName, aFamilyName, aEmail, aAddress, aPhone, aUserName, aPassword, res);
 });
 
 app.post('/confirm', function(req, res){
-    res.sendFile(path.join(__dirname, 'login.html'));
+    //res.sendFile(path.join(__dirname, 'login.html'));
 
     var aVerify = req.body.verify;
     console.log("Verify code" + aVerify);
-    ConfirmUser(aVerify);
+    ConfirmUser(aVerify, res);
 });
 
 
-function register(name, familyName, email, address, phone, username, password)
+function register(name, familyName, email, address, phone, username, password, res)
 {
    
-
+    //used to store attributes for registered user
     var attributeList = [];
 
     var dataEmail = {
@@ -104,43 +114,55 @@ function register(name, familyName, email, address, phone, username, password)
         Name : 'address',
         Value : address
     };
+
+    //attributes given in register form
     var attributeEmail = new CognitoUserAttribute(dataEmail);
     var attributePhoneNumber = new CognitoUserAttribute(dataPhoneNumber);
     var attribueFamilyName = new CognitoUserAttribute(dataFamilyName);
     var attributeName = new CognitoUserAttribute(dataName);
     var attributeAddress = new CognitoUserAttribute(dataAddress);
 
+    //add attributes to attribute list
     attributeList.push(attributeEmail);
     attributeList.push(attributePhoneNumber);
     attributeList.push(attribueFamilyName);
     attributeList.push(attributeName);
     attributeList.push(attributeAddress);
 
+    //add username, password and attributes to user pool
     userPool.signUp(username, password, attributeList, null, function(err, result){
         if (err) {
            console.log(err);
+           res.sendFile(path.join(__dirname, 'register.html'));
         }
         else
         {
-        cognitoUser = result.user;
-        console.log('user name is ' + cognitoUser.getUsername());
+            //user added to user pool
+            cognitoUser = result.user;
+            console.log('user name is ' + cognitoUser.getUsername());
+            res.sendFile(path.join(__dirname, 'confirm.html'));
        }
     });
 }
 
-function ConfirmUser(code){
+function ConfirmUser(code, res){
+
+    //give verify code to cognito user
      cognitoUser.confirmRegistration(code, true, function(err, result) {
     if (err) 
     {
-       console.log(err);
-        return;
+        console.log(err);
+        res.sendFile(path.join(__dirname, 'confirm.html'));
     }
-    console.log('call result: ' + result);
+    else{
+        console.log('call result: ' + result);
+        res.sendFile(path.join(__dirname, 'login.html'));
+    }
 });
 }
 
-function Login(userName, password){
-    var authenticationData = {
+function Login(userName, password, res){
+        var authenticationData = {
         Username : userName, // your username here
         Password : password, // your password here
     };
@@ -148,17 +170,23 @@ function Login(userName, password){
     var userData = { 
         Username: userName,
         Pool: userPool
-        };
+    };
     var authenticationDetails = new AuthenticationDetails(authenticationData);
  
     var cognitoUser = new CognitoUser(userData);
+
+    //check user name and password match a user in userpool
     cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: function (result) {
             console.log('access token + ' + result.getAccessToken().getJwtToken());
+
+            res.sendFile(path.join(__dirname, 'welcome.html'));
         },
  
         onFailure: function(err) {
             console.log(err);
+
+            res.sendFile(path.join(__dirname, 'login.html'));
         },
         mfaRequired: function(codeDeliveryDetails) {
             var verificationCode = prompt('Please input verification code' ,'');
