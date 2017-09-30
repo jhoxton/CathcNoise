5b47b2fae9c3d6a48532dd86670255420e240550
jhoxton <jakehnovak@gmail.com>
2017-09-30 18:55:03 +1000

    Update catchnoise.js
    Added notification icon script

--- js/catchnoise.js
+++ js/catchnoise.js
@@ -18,7 +18,20 @@ function addEvent() //This will be the function that takes data from the Dynamo 
 		cel3.innerHTML = time;
 	}
 	
+	window.onLoad = updateNotif(newEvent);
+	var newEvent = true;//When a new notification arrives, this will become true
 	
+	function updateEvent() {
+	//A function to check for a new notification
+	}
+	
+	function updateNotif(newEvent) {//If true, display new notif
+		if (newEvent = true) {
+			document.getElementById("imgNotifId").src ="images/notifTest.png";
+		} else {
+			document.getElementById("imgNotifId").src ="images/notif.png";
+		}
+	}
 	
 // function openCity(evt, cityName) {
 //     var i, tabcontent, tablinks;
