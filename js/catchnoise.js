9c5f0889b5b1f4536e08cac060b276c6b90404eb
jhoxton <jakehnovak@gmail.com>
2017-10-06 14:56:47 +1100

    Add files via upload

--- js/catchnoise.js
+++ js/catchnoise.js
@@ -1,40 +1,56 @@
-function addEvent() //This will be the function that takes data from the Dynamo DB and writes it to the webpage
-	{
-		//This just prints the current time and date
-		var date = new Date();
-		var n = date.toDateString();
-		var time = date.toLocaleTimeString();
-		var id =document.getElementById('id').value;// This will be the device ID
-		var db =document.getElementById('db').value; //This will be the recorded volume
-		// var time =document.getElementById('time').value; //This is just a "print string" function for the date tab. Probably won't need it
-		var time =date; //This just prints the current time and date
-		var table = document.getElementsByTagName('table') [0];
-		var newRow = table.insertRow(1);
-		var cel1 = newRow.insertCell(0);
-		var cel2 = newRow.insertCell(1);
-		var cel3 = newRow.insertCell(2);
-		cel1.innerHTML = id;
-		cel2.innerHTML = db;
-		cel3.innerHTML = time;
-	}
-	
-	window.onLoad = updateNotif(newEvent);
-	var newEvent = true;//When a new notification arrives, this will become true
-	
-	function updateEvent() {
-	//A function to check for a new notification
-	}
-	
-	function updateNotif(newEvent) {//If true, display new notif
-		if (newEvent = true) {
-			document.getElementById("imgNotifId").src ="images/notifTest.png";
-		} else {
-			document.getElementById("imgNotifId").src ="images/notif.png";
-		}
-	}
-	var number = "123";
-    document.getElementById("TEST").innerHTML = number;
-	
+
+function printEvents(eventList) //This will be the function that takes data from the Dynamo DB and writes it to the webpage
+{
+    //This just prints the current time and date
+    // var date = new Date();
+    // var n = date.toDateString();
+    // var time = date.toLocaleTimeString();
+
+    var id = document.getElementById('id').value; // This will be the device ID
+    var db = document.getElementById('db').value; //This will be the recorded volume
+    // var time =document.getElementById('time').value; //This is just a "print string" function for the date tab. Probably won't need it
+    var time = date; //This just prints the current time and date
+    var table = document.getElementsByTagName('table')[0];
+    var newRow = table.insertRow(1);
+    var cel1 = newRow.insertCell(0);
+    var cel2 = newRow.insertCell(1);
+    var cel3 = newRow.insertCell(2);
+    cel1.innerHTML = id;
+    cel2.innerHTML = db;
+    cel3.innerHTML = time;
+}
+
+function printEvents(eventList) {
+	 // document.getElementById("TEST").innerHTML =eventList[0].EventID;//TEST ID PRINT
+	 
+    for (var i = eventList.length - 1; i >= 0; i--) {
+        // console.log("JS, array's eventID is", eventList[i].EventID); //Prints eventID's in the arraylist
+        //  console.log("JS, array's timeStamp is", eventList[i].TimeStamp);
+
+        document.getElementById("id").innerHTML = eventList[i].EventID;
+        document.getElementById("timeStamp").innerHTML = eventList[i].TimeStamp;
+        document.getElementById("type").innerHTML = eventList[i].SourceType;
+        document.getElementById("db").innerHTML = eventList[i].dB_level;
+
+    };
+}
+
+window.onLoad = updateNotif(newEvent);
+var newEvent = true; //When a new notification arrives, this will become true
+
+function updateEvent() {
+    //A function to check for a new notification
+}
+
+function updateNotif(newEvent) { //If true, display new notif
+    // if (newEvent = true) {
+    // 	document.getElementById("imgNotifId").src ="images/notifTest.png";
+    // } else {
+    // 	document.getElementById("imgNotifId").src ="images/notif.png";
+    // }
+}
+
+
 // function openCity(evt, cityName) {
 //     var i, tabcontent, tablinks;
 //     tabcontent = document.getElementsByClassName("tabcontent");
@@ -50,4 +66,4 @@ function addEvent() //This will be the function that takes data from the Dynamo 
 // }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// document.getElementById("defaultOpen").click();
--- js/dbConnect.js
+++ js/dbConnect.js
@@ -4,9 +4,9 @@
 // console.log(CONFIG);
 
 var dynamodb = new AWS.DynamoDB({
-	region: "ap-southeast-2",
-	accessKeyId: CONFIG.AWS_ACCESS_KEY,
-	secretAccessKey: CONFIG.AWS_SECRET_KEY
+    region: "ap-southeast-2",
+    accessKeyId: CONFIG.AWS_ACCESS_KEY,
+    secretAccessKey: CONFIG.AWS_SECRET_KEY
 });
 
 
@@ -25,69 +25,148 @@ var dynamodb = new AWS.DynamoDB({
 // });
 
 
-console.log("GETTING SHIT FROM DATA");
-	
 
-var getAllEvents =  {
-	TableName: 'Events',
-	Select: 'SPECIFIC_ATTRIBUTES',
-	AttributesToGet: [
-		'EventID',
-		'Timestamp',
-		'SourceType',
-		'dB_level'
-	]
+
+var getAllEvents = {
+    TableName: 'Events',
+    Select: 'SPECIFIC_ATTRIBUTES',
+    AttributesToGet: [
+        'EventID',
+        'Timestamp',
+        'SourceType',
+        'dB_level'
+    ]
 
 };
 
 var loopEvent = {
-	EventID: "",
-	Timestamp: "",
-	SourceType: "",
-	dB_level: ""
+    EventID: "",
+    Timestamp: "",
+    SourceType: "",
+    dB_level: ""
 };
+var eventList = [];
 
 
-	var testEventID;
-// SCAN FUNCTION GETS ALL ITEMS IN ENTIRE TABLE
-dynamodb.scan(getAllEvents, function(err, data) {
-	if (err) { 	
-		console.log(err, err.stack);
-	} else {
+var testEventID;
 
-		for (var i = data.Items.length - 1; i >= 0; i--) { //Loops over each event and prints the EventID
-			
-			// data.Items[i]
-			// console.log(i);
-			
-			// var j = data.Items[i].EventID.S;
-			// console.log(j);
+dynamodb.scan(getAllEvents, function(err, data) { // SCAN FUNCTION GETS ALL ITEMS IN ENTIRE TABLE
+    if (err) {
+        console.log(err, err.stack);
+    } else {
+        // console.log("db input =", data.dB_level);
+        for (var i = data.Items.length - 1; i >= 0; i--) { //Loops over each event and prints the EventID
+            // for (var i = 0; i < data.Items.length; i++) {
+// console.log("Total events = ",data.Items.length);
+            // console.log("Data in Scan",data.Items[i]);
+            loopEvent.EventID = data.Items[i].EventID.S; //Adds the parameters to a the loopEvent
 
 
-			loopEvent.EventID = data.Items[i].EventID.S; //Adds the EventID to a the loopEvent
-			loopEvent.Timestamp = data.Items[i].Timestamp.S;
-			loopEvent.SourceType = data.Items[i].SourceType.S;
-			loopEvent.dB_level = data.Items[i].dB_level.S;
-
-			var addEvent = loopEvent;
-
-			console.log("loopEvent's ID is ", loopEvent.EventID);//Test log
-			console.log("addEvent's ID is ", addEvent.EventID);//Test log
-		}
-
-		// testEventID= data.Items[0].EventID.S;
-		// console.log(testEventID); //Tests the EventID became a string
-
-		
-    	document.getElementById("TEST").innerHTML = testEventID;
+            loopEvent.Timestamp = data.Items[i].Timestamp.S;
 
 
+            loopEvent.SourceType = data.Items[i].SourceType.S;
 
-		//MAKE SURE TYPE IS S OR N FOR STRING OR NUMBER
-		//MAKE ARRAY OF GLOBAL OBJECTS TO PRINT TO EVENTS TABLE
-	}
+
+            loopEvent.dB_level = data.Items[i].dB_level.N;
+
+            // TESTING BLOCK
+            console.log("ITEMS db = ", data.Items[i].dB_level.S);
+            console.log("ITEMS Event = ", data.Items[i].EventID.S);
+            console.log("ITEMS Time = ", data.Items[i].Timestamp.S);
+            console.log("ITEMS Source = ", data.Items[i].SourceType.S);
+
+            console.log("LOOPEVENT Event = ", loopEvent.EventID);
+            console.log("LOOPEVENT Time = ", loopEvent.Timestamp);
+            console.log("LOOPEVENT Source = ", loopEvent.SourceType);
+            console.log("LOOPEVENT db = ", loopEvent.dB_level);
+
+            var addEvent = loopEvent;
+      
+            console.log("ADDEVENT Event = ", addEvent.EventID);
+            console.log("ADDEVENT Time = ", addEvent.Timestamp);
+            console.log("ADDEVENT Source = ", addEvent.SourceType);
+            console.log("ADDEVENT db = ", addEvent.dB_level);
+            eventList.push(addEvent, eventList.length);
+
+         //    for (var i = eventList.length - 1; i >= 0; i--) {
+         //    console.log("Time stamp in array =", eventList[i].Timestamp);
+        	// }
+        }
+        // for (var i = eventList.length - 1; i >= 0; i--) {
+        //     console.log("ID in array =", eventList[i].EventID);
+        // 		}
+        // for (var i = data.Items.length - 1; i >= 0; i--) { //Loops over each event and prints the EventID
+        // 	// createEvents();
+        // 	data.Items[i];
+        // 	console.log(data.Items[i].dB_level);
+        // }
+        // 	// var j = data.Items[i].EventID.S;
+        // 	// console.log(j);
+
+
+        // 	loopEvent.EventID = data.Items[i].EventID.S; //Adds the EventID to a the loopEvent
+        // 	loopEvent.Timestamp = data.Items[i].Timestamp.S;
+        // 	loopEvent.SourceType = data.Items[i].SourceType.S;
+        // 	loopEvent.dB_level = data.Items[i].dB_level.S;
+
+        // 	var addEvent = loopEvent;
+
+        // 	console.log("loopEvent's ID is ", loopEvent.EventID);//Test log
+        // 	console.log("addEvent's ID is ", addEvent.EventID);//Test log
+
+
+        // testEventID= data.Items[0].EventID.S;
+        // console.log(testEventID); //Tests the EventID became a string
+
+        // createEvents(getAllEvents, data); //Passes the data object to a new function to create JS events
+
+
+
+
+        //MAKE SURE TYPE IS S OR N FOR STRING OR NUMBER
+        //MAKE ARRAY OF GLOBAL OBJECTS TO PRINT TO EVENTS TABLE
+    }
 });
 
-function createEvents() {
+function createEvents(getAllEvents, data) {
+    for (var i = data.Items.length - 1; i >= 0; i--) { //Loops over each event and prints the EventID
+        // for (var i = 0; i < data.Items.length; i++) {
+        console.log("Data in create items", data.Items[i]);
 
+        // data.Items[i]
+        // console.log(i);
+
+        // var j = data.Items[i].EventID.S;
+        // console.log(j);
+
+
+        loopEvent.EventID = data.Items[i].EventID.S; //Adds the parameters to a the loopEvent
+        // console.log("Event = ",data.Items[i].EventID.S);
+        loopEvent.Timestamp = data.Items[i].Timestamp.S;
+        // console.log("Time = ",data.Items[i].Timestamp.S);
+        loopEvent.SourceType = data.Items[i].SourceType.S;
+        // console.log("Source = ",data.Items[i].SourceType.S);
+
+        loopEvent.dB_level = data.Items[i].dB_level.N;
+        // console.log("db = ",data.Items[i].dB_level.S);
+        var addEvent = loopEvent;
+        console.log("Time input= ", addEvent.Timestamp);
+
+        // document.getElementById("TEST").innerHTML = addEvent.EventID;//TEST ID PRINT
+        eventList.push(addEvent);
+        for (var i = eventList.length - 1; i >= 0; i--) {
+            console.log("Time stamp in array =", eventList[i].Timestamp);
+        }
+
+        // console.log("loopEvent's ID is ", loopEvent.EventID);//Test loopEvent log
+        // console.log("addEvent's ID is ", addEvent.EventID);//Test addEvent log
+
+    }
+    printEvents(eventList); //Passes on the array
 }
+// function printEvents(eventList) {
+// 	for (var i = eventList.length - 1; i >= 0; i--) {
+// 		console.log("Array's eventID is" , eventList[i].EventID);//Prints eventID's in the arraylist
+// 	};
+// }
